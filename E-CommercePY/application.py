6eab2c3430cd57cb8db4c2153f65a1a493c7f39f
Ledbrozzer <josemariosc01@gmail.com>
2024-08-10 23:10:58 -0300

    API-Notes-Study

---
+++ E-CommercePY/application.py
@@ -0,0 +1,21 @@
+#criar pasta EB-API
+#Criar arquivo: 'application.py' e 'requirements.txt'
+
+
+#  EB-API/application.py:
+
+#from flask import Flask
+
+#application = Flask(__name__)
+
+#@application.route('/')
+#def Ceia():
+#    return 'Hoje tem Santa Ceia'
+
+#if __name__ == "__main__":
+#    application.run(debug=True)
+
+
+#  EB-API/requirements.txt:
+
+# Flask==2.3.0
---
+++ E-CommercePY/support.py
@@ -0,0 +1,159 @@
+#Notes->study/estudo
+#app instancia rota do Flask
+
+#http://127.0.0.1:5000
+#if __name__ == "__main__":
+#    app.run(debug=True)
+    #Não ficar rodando servidor novo a cada importação
+# ctrl+c
+
+#flask shell
+#db.create_all()
+#db.session.commit()
+#exit()
+
+#extensions
+#SQLite Viewer
+
+#{
+#    "name": "TV ",
+#    "price": 0,
+#    "description": "Descrição Teste"
+#}
+
+
+#python -m flask shell
+#db.drop_all()
+#db.create_all()
+#db.session.commit()
+#Use exit() or Ctrl-Z plus Return to exit
+#exit()
+
+#python -m flask shell
+#user = User(username="admin", password="123")
+#user
+#user.id
+#db.session.add(user)
+#db.session.commit()
+#exit()
+
+#python -m flask shell
+#db.drop_all()
+#db.create_all()
+#user = User(username="admin", password="123")
+#db.session.add(user)
+#db.session.commit()
+#exit()
+
+#
+#
+#
+#
+#
+#python.exe -m pip install --upgrade pip
+
+#python -m flask shell
+
+
+#python3 -m ensurepip
+
+
+#pip3 install -r requisitions.txt
+
+#pip3 install -r requirements.txt
+
+#cd "C:\Users\josem\OneDrive\Documentos\E-CommercePY"
+
+#npm install pip3
+
+#npm help
+
+#ls
+
+
+
+
+#cd aws-elastic-beanstalk-cli-setup-master
+#ls
+#cd scripts
+#ls
+#python3 ebcli_installer.py
+
+#eb --version
+
+#eb init -p python-3.11 api-flask --region us-east-1
+#eb create flask-env-dev
+#eb terminate flask-env-dev
+
+#eb init -p python-3.11 ecommerce-api--region us-east-1
+#eb create api-dev
+
+#eb open api-dev
+
+#eb deploy api-dev
+#eb terminate api-dev
+
+
+
+
+#C:\Program Files (x86)\sqlite3
+
+#git init
+
+#git status
+
+#git add .
+
+#git config --global user.email ""
+
+#git config --global user.name ""
+
+
+#git commit -m "config: Starting Project with init dir"
+
+#pip install virtualenv
+#////
+#pip3 install virtualenv
+
+#python3 -m venv venv
+
+#cd venv
+
+#source venv/bin/activate
+
+#. venv/bin/activate
+
+#python.exe -m pip install --upgrade pip
+
+#pip3 install Flask
+#////
+#pip install Flask
+
+#python3 run.py
+
+#cd ..
+
+#git status
+
+#git add .
+
+#git commit -m "configs: Adding framework to Project and config .vscode"
+
+#python3 run.py
+
+#pip show flask
+
+#pip install flask
+
+
+#cd /path////
+
+#Ativar ambiente virtual no windows:
+#source venv/Scripts/activate
+
+#Ativar ambiente virtual no Linux ou macOS:
+#source venv/bin/activate
+
+
+
+
